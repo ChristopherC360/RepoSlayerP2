30999cea05132afb20c86a234d892105a6fa7a34
RealmsEnd <83333157+RealmsEnd@users.noreply.github.com>
2022-02-21 13:38:01 -0600

    added filler files

--- src/main/java/com/reposlayers/SpringBootInsuranceApp/Project2Application.java
+++ src/main/java/com/reposlayers/SpringBootInsuranceApp/Project2Application.java
@@ -2,8 +2,14 @@ package com.reposlayers.SpringBootInsuranceApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.reposlayers.repositories")
+@EntityScan("com.reposlayers.models")
+@ComponentScan("com.reposlayers")
 public class Project2Application {
 
 	public static void main(String[] args) {
